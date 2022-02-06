9ce570bfe9c5027b05c17ae66cab2b51118958e3
Natnael Tekletsadik <natnaeltekletsadik@Natnaels-MacBook-Air.local>
2022-02-05 21:30:25 -0500

    apply fonts

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,16 +4,18 @@ const Home = () => {
 
   return (
     <div className="home">
+
       <nav className="nav-bar">
-        <div className="logo">Remind.Me</div>
+        <div className="logo"><strong>Remind.Me</strong></div>
         <div className="right">
           <p className="sign-up">Sign Up</p>
           <p className="log-in">Log In</p>
         </div>
       </nav>
+
       <section className="main">
         <div className="introduction">
-          <h2 className="now" >DO It later Now !</h2>
+          <p className="now" >DO It <strike>later</strike>  Now !</p>
           <p className="about-app">
           She didn't understand how change worked. When she looked at today compared to yesterday, there was nothing that she could see that was different.
 
@@ -28,12 +30,20 @@ const Home = () => {
           Yet, when she looked at today compared to last year, she couldn't see how anything was ever the same.
           </p>
         </div>
-        <div className="image">
-          Image will be here
-        </div>
+        <i className="fas fa-sms fa-9x"></i>
+
       </section>
+
+      <div className="get-started" >
+        <button className="cr-ac" >Create Account</button>
+        <p className="or">or</p>
+        <button className="log-in" >Log In</button>
+      </div>
+
     </div>
     );
 }
 
 export default Home;
+
+
