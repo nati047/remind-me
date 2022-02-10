5c268ef52cb0fc64a40ab9e7d6c0edc41fc2259f
Natnael Tekletsadik <natnaeltekletsadik@Natnaels-MacBook-Air.local>
2022-02-10 01:08:49 -0500

    finsh work on task style: except font

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -13,9 +13,9 @@ function Task() {
           <h4 className='task-type'>daily</h4>
         </div>
         <div className="task-footer-right">
-          <h3 className='complete'>Complete</h3>
+          <h3 className='complete button'>Complete</h3>
           {/* <button className='restart'>Restart</button> */}
-          <h3 className='delete'>Delete</h3>
+          <h3 className='delete button'>Delete</h3>
         </div>
       </div>
     </div>
