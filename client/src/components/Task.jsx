cfec515d54db3429068f5aebb829159c60dd080e
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-05-28 08:55:56 -0400

    apply styling to task component

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -3,19 +3,19 @@ import '../styles/Task.css';
 import { Button } from 'react-bootstrap';
 function Task() {
   return(
-    <div className='task'>
+    <div className='task-container'>
       <div className='task-header' >
-        <h1 className='task-name'>Take Vitamins</h1>
-        <h3 className='task-status'>Active</h3>
+        <p className='task-name'>Take Vitamins</p>
+        <p className='task-status'>Active</p>
       </div>
       <div className='task-footer'>
         <div className="task-footer-left">
-          <h4 className='task-time'>5:00 pm</h4>
-          <h4 className='task-type'>daily</h4>
+          <p className='task-time'>5:00 pm</p>
+          <p className='task-type'>daily</p>
         </div>
         <div className="task-footer-right">
           <button className='complete task-button'>Complete</button>
-          {/* <button className='restart'>Restart</button> */}
+          {/* <button className='restart task-button'>Restart</button> */}
           <button className='delete task-button'>Delete</button>
         </div>
       </div>
--- client/src/components/Tasks.jsx
+++ client/src/components/Tasks.jsx
@@ -5,10 +5,21 @@ import '../styles/Tasks.css'
 import { Button, Container, Col, Row } from 'react-bootstrap'; 
 
 function Tasks() {
+  
+  const handleClick = (event) => {
+    event.toggleClass('active')
+  }
+
   return (
     <div className='tasks-list'>
       <Nav className='dashboard'/>
-        <div className="select-frequency"></div>
+      <div onClick={handleClick} className="select-frequency">
+        <div className="frequency">all</div>
+        <div className="frequency">once</div>
+        <div className="frequency">daily</div>
+        <div className="frequency">weekly</div> 
+        <div className="frequency">monthly</div>
+      </div>
       <Container fluid style={{ marginTop: "0px"}}>
         <Row>
         <Col lg={4} style={{ marginTop: "0px"}}>
