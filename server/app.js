2c803b26206a07f1d519b86461f8b97d733a95c4
Natnael Tekletsadik <isaacnatnael@gmail.com>
2022-06-03 09:04:33 -0400

    setup api endpoints and test

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 const express  = require('express');
+// const router = require('express').Router();
 const app = express();
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 2020;
 require('dotenv').config();
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send('<h1>Hola</h1>')
-})
+// mount routers 
+app.use("/auth", require("./auth"));
+app.use("/api", require("./api"));
 
-app.listen(2020, () => {
+
+app.listen(PORT, () => {
   console.log('server listening')
 })
---
+++ server/auth/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { User } = require('../db/models');
+
+router.get("/login", (req, res) => {
+  res.send('login api')
+});
+
+router.post("/register", (req, res) => {
+  res.send('login page')
+});
+
+module.exports = router;
